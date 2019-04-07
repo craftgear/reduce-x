94271214809eeddb831bcfeb5d78e18cf05470bb
Shunsuke Watanabe <craftgear@gmail.com>
2019-04-08 08:22:09 +0900

    add specs

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,4 +1,12 @@
-import { curry /* , reduceSlideWindow, reduceChunk, reduceSplit, reduceAsync */ } from './index';
+import {
+  curry,
+  reduceSlideWindow,
+  reduceChunk,
+  reduceSplit,
+  reduceAsync,
+  sleep,
+  reduceObject,
+} from './index';
 
 describe('curry', () => {
   it('curry functions', () => {
@@ -10,6 +18,62 @@ describe('curry', () => {
   });
 });
 
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+const sum3 = (accum, [x, y, z]) => [...accum, x + y + z];
+
 describe('reduceSlideWindow', () => {
-  it('pick N items each time then slice M items', () => {});
+  it('pick 3 items each time then slide 1 item', () => {
+    const result = reduceSlideWindow(sum3, [], 3, 1, numbers);
+    expect(result).toEqual([6, 9, 12, 15, 18, 21, 24, 27, 30]);
+  });
+
+  it('pick 3 items each time then slide 2 item', () => {
+    const result = reduceSlideWindow(sum3, [], 3, 2, numbers);
+    expect(result).toEqual([6, 12, 18, 24, 30]);
+  });
+
+  it('pick 3 items each time then slide 3 item', () => {
+    const result = reduceSlideWindow(sum3, [], 3, 3, numbers);
+    expect(result).toEqual([6, 15, 24]);
+  });
+});
+
+describe('reduceChunk', () => {
+  it('takes 3 items each iteration then increase index by 1', () => {
+    const result = reduceChunk(sum3, [], 3, numbers);
+    expect(result).toEqual([6, 9, 12, 15, 18, 21, 24, 27, 30]);
+  });
+});
+
+describe('reduceSplit', () => {
+  it('takes 3 items at once then repeat, does not iterate on items less than split size', () => {
+    const result = reduceSplit(sum3, [], 3, numbers);
+    expect(result).toEqual([6, 15, 24]);
+  });
+});
+
+describe('reduceAsync', () => {
+  it('runs async functions as iterators', async () => {
+    const result = reduceAsync(
+      async (accum, curr) => {
+        await sleep(10);
+        return accum + curr;
+      },
+      0,
+      [1, 2, 3, 4, 5]
+    );
+    expect(result).resolves.toEqual(15);
+  });
+});
+
+describe('reduceObject', () => {
+  it('reduce an object into key/value pairs', () => {
+    const o = {
+      name: 'hoge',
+      count: 3,
+    };
+
+    const result = reduceObject((accum, [key, value]) => [...accum, `${key} is ${value}`], [], o);
+    expect(result).toEqual(['name is hoge', 'count is 3']);
+  });
 });
