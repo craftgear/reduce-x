b7668c73b9cd78fc1ea4ea41f9013a505611d864
Shunsuke Watanabe <craftgear@gmail.com>
2019-04-09 08:01:44 +0900

    build

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,6 +1,6 @@
 export declare const curry: (fn: Function) => Function;
 export declare const sleep: (ms?: number) => Promise<void>;
-export declare const reduceSlideWindow: <T>(fn: Function, init: T, windowSize: number, stride: number, arrayData: any[]) => T;
+export declare const reduceSlideWindow: <T>(fn: Function, init: T, kernel: number, stride: number, arrayData: any[]) => T;
 export declare const reduceChunk: Function;
 export declare const reduceSplit: Function;
 export declare const reduceAsync: Function;
